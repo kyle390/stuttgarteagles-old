1a5e991af3451d90a51c2feabc2e91b8c9be3af0
Scott Underwood <nmoutdoors@gmail.com>
2016-01-31 20:52:47 +0100

    Many Changes and Such...
    Uhh... I should've broke it up a bit more...

---
+++ js/eagles.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+$("i").addClass("animated bounce");
+$("#box1").addClass("animated fadeInLeft")
+$("#box2").addClass("animated fadeInDown")
+$("#box3").addClass("animated fadeInRight")
+$("#box1-2").addClass("animated fadeInLeft")
+$("#box2-2").addClass("animated fadeInUp")
+$("#box3-2").addClass("animated fadeInRight")
+});
