1e49d3ce296ba5dde69955fc37c9e19b805da8c1
Scott Underwood <nmoutdoors@gmail.com>
2016-01-31 21:13:40 +0100

    JS Updates

--- js/eagles.js
+++ js/eagles.js
@@ -1,9 +1,27 @@
 $(document).ready(function() {
 $("i").addClass("animated bounce");
-$("#box1").addClass("animated fadeInLeft")
-$("#box2").addClass("animated fadeInDown")
-$("#box3").addClass("animated fadeInRight")
-$("#box1-2").addClass("animated fadeInLeft")
-$("#box2-2").addClass("animated fadeInUp")
-$("#box3-2").addClass("animated fadeInRight")
+$("#box1").addClass("animated fadeInLeft");
+$("#box1-title").text("First Title");
+$("#box1-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box2").addClass("animated fadeInDown");
+$("#box2-title").text("Second Title");
+$("#box2-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box3").addClass("animated fadeInRight");
+$("#box3-title").text("Third Title");
+$("#box3-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box1-2").addClass("animated fadeInLeft");
+$("#box1-2-title").text("Lower Left Title");
+$("#box1-2-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box2-2").addClass("animated fadeInUp");
+$("#box2-2-title").text("Bottom Middle Title");
+$("#box2-2-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
+$("#box3-2").addClass("animated fadeInRight");
+$("#box3-2-title").text("Lower Right Title");
+$("#box3-2-body").text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean dictum magna venenatis purus porta, sed laoreet enim ullamcorper.");
+
 });
